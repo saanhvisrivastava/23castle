424d48ee404a37e58205dec03a5db9b2e8f48a2c
saanhvisrivastava <63591636+saanhvisrivastava@users.noreply.github.com>
2020-05-20 20:36:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(800,400);
+  var star=createSprite(200, 100, 275, 300, 90, 175, 310, 175, 125, 300);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
